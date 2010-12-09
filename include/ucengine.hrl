5b8979f05cc2a7b8ceb317139f05106fb8859491
phorque <victor.goya@af83.com>
2010-12-09 18:36:46 +0100

    updated events name

--- include/ucengine.hrl
+++ include/ucengine.hrl
@@ -18,8 +18,8 @@
           %% MetaData : list
           metadata = []}).
 
--define(UCE_MEETING_JOIN_EVENT, "internal.meeting.join").
--define(UCE_MEETING_LEAVE_EVENT, "internal.meeting.leave").
+-define(UCE_MEETING_JOIN_EVENT, "internal.roster.add").
+-define(UCE_MEETING_LEAVE_EVENT, "internal.roster.delete").
 -define(UCE_STREAM_NEW_EVENT, "video.stream.new").
 -define(UCE_STREAM_STOP_EVENT, "video.stream.stop").
 -define(UCE_STREAM_START_EVENT, "video.stream.start").
